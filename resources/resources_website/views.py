6dfc6e19ce47a408747fbb9fdd3ef50ce2a1bc34
Maciej Ociepa <58308074+przemilymaciej@users.noreply.github.com>
2022-12-06 19:45:02 +0100

    Add rows to the table dynamically from the database

--- resources/resources_website/views.py
+++ resources/resources_website/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+from .models import Resource
+
+class IndexView(generic.ListView):
+    model = Resource
+    template_name = 'resources_website/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['resources'] = Resource.objects.order_by('resource_id')
+        return context
