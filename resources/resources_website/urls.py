297bdce4c9393246062b516c9cda36932719c54b
Maciej Ociepa <58308074+przemilymaciej@users.noreply.github.com>
2022-12-24 16:11:26 +0100

    release request has been prepared using ajax

--- resources/resources_website/urls.py
+++ resources/resources_website/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    # path('release_resources', views.AjaxHandlerView.as_view(), name='release_resources'),
+    path('release_resources', views.release_resources, name='release_resources'),
 ]
--- resources/resources_website/views.py
+++ resources/resources_website/views.py
@@ -1,7 +1,11 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.views.generic import View
+from django.http.response import JsonResponse
+from django.shortcuts import render, redirect
+
 
 from .models import Resource
 from .filters import ResourceFilter
@@ -42,3 +46,35 @@ class IndexView(generic.ListView):
         context['resources'] = resources
         context['myFilter'] = myFilter
         return context
+
+
+# class AjaxHandlerView(View):
+#     def post(self, request):
+#         id = request.GET.get('id')
+#         print(id)
+#         return render(request, 'resources_website/index.html')
+
+# def release_resources(request):
+#     # print(request.GET['id'])
+#     print("sialala")
+
+# def release_resources(request):
+#     if request.method == 'GET':
+#            post_id = request.GET['post_id']
+#            return HttpResponse("Success!") # Sending an success response
+#     else:
+#            return HttpResponse("Request method is not a GET")
+
+def release_resources(request):
+    if request.method == 'POST':
+        items = request.POST.get('items')
+        to_find = items.split(',')[1:]
+
+        for id in to_find:
+            el = Resource.objects.get(pk=id)
+            el.request_id = None
+            el.save()
+
+        return JsonResponse({'status': 'Resource released'})
+
+    return redirect('/')
